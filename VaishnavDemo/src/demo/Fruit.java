830bb1807778922aaac255ff8839a5f57ad87c91
DeepikaJeyaganesh <kdeepika101@gmail.com>
2023-10-27 19:23:36 +0530

    second commit

--- VaishnavDemo/src/demo/Fruit.java
+++ VaishnavDemo/src/demo/Fruit.java
@@ -4,7 +4,7 @@ public class Fruit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("Hello world, have a nice day");
 	}
 
 }
